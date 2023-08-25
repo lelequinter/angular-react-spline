3d11e93a07fd7fd6a01b5e7c35435ffb5180588e
lelequinter <lele.quinter@gmail.com>
2023-08-25 16:41:45 -0500

    agregar el spline viewer al componente de React

--- src/app/components/spline-viewer.tsx
+++ src/app/components/spline-viewer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import Spline from '@splinetool/react-spline';
 import { useState } from "react";
 
 const SplineViewer = () => {
@@ -15,6 +15,8 @@ const SplineViewer = () => {
       { text }
 
       <button onClick={onChangeText} > Cambiar texto </button>
+      <Spline scene="https://prod.spline.design/U9O6K7fXziMEU7Wu/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/UWoeqiir20o49Dah/scene.splinecode" />
     </div>
   )
 }
