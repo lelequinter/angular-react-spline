62a102414d558ff966cf2d2468fc834daad0b88d
lelequinter <lele.quinter@gmail.com>
2023-08-25 16:12:45 -0500

    renderizar el componente de react en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import SplineViewer from "./components/spline-viewer";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnChanges, AfterViewInit, OnDestroy {
+
   title = 'angular-react-spline';
+
+  public reactComponentId = 'reactComponnet';
+
+  ngOnChanges(changes: SimpleChanges){
+    this.render();
+  }
+
+  ngAfterViewInit(){
+    this.render();
+  }
+
+  ngOnDestroy(){
+
+  }
+
+  private render(){
+    ReactDOM.render(React.createElement(SplineViewer), document.getElementById(this.reactComponentId));
+  }
 }
